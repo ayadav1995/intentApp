224894d47adf102bb41f293d16eba32c67a312d8
Checkout <abhishek.yadav02@sjsu.edu>
2022-03-26 22:39:56 -0700

    call func added

--- app/src/main/java/com/example/implicitintentapp/MainActivity.java
+++ app/src/main/java/com/example/implicitintentapp/MainActivity.java
@@ -1,18 +1,26 @@
 package com.example.implicitintentapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     EditText editText;
     Button btn;
+    EditText num;
+    Button callButton;
+    String sNum;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +31,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         editText = findViewById(R.id.url);
         btn = findViewById(R.id.urlButton);
         btn.setOnClickListener(this);
+        callButton = findViewById(R.id.callButton);
+        num = findViewById(R.id.number);
+        callButton.setOnClickListener(this::btnCall);
 
     }
 
@@ -35,4 +46,28 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         Intent intent1 = Intent.createChooser(intent,"Open with");
         startActivity(intent1);
     }
+
+    public void btnCall(View view){
+        Intent i = new Intent(Intent.ACTION_CALL);
+        sNum = num.getText().toString();
+
+        if(sNum.trim().isEmpty()){
+            i.setData(Uri.parse("tel:11111111111"));
+
+        }else{
+            i.setData(Uri.parse("tel:" +sNum));
+        }
+
+        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED){
+            Toast.makeText(this,"Please grant the permission to call", Toast.LENGTH_SHORT).show();
+            requestPermission();
+        }else{
+            startActivity(i);
+        }
+    }
+
+    private void requestPermission(){
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CALL_PHONE},1);
+    }
+
 }
